2d77fc1dcc5cf811b0df7d76c4afb9a86bd3c290
Stefan B <stefanbolcs@gmail.com>
2020-11-23 21:54:11 +0100

    login with error handling by observable

--- auth-01-auth-component/src/app/auth/auth.service.ts
+++ auth-01-auth-component/src/app/auth/auth.service.ts
@@ -7,13 +7,14 @@ import { throwError } from 'rxjs';
 
 
 
-interface AuthResponseData {
+export interface AuthResponseData {
 kind: string;
 idToken:string;
 email: string;
 refreshToken:string;
 expiresIn:string;
 localId:string;
+registered?:boolean;
 }
 
 
@@ -39,4 +40,17 @@ export class AuthService {
               return throwError(errorMessage);
         }));
     }
-}
+
+    //sign in with firebase rest api authentication
+    login(email:string, password:string){
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAvrVvXu_a9DEo-XTE22X9onDE1t6f3dJo',
+        {email: email,
+        password: password,
+        returnSecureToken:true 
+    }
+    );
+    }
+    }
+
+
+
