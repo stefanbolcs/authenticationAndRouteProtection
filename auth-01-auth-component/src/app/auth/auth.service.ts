cea45909a33c212242a96b8684c9f3fae86c997c
Stefan B <stefanbolcs@gmail.com>
2020-11-25 21:57:24 +0100

    implementing autoLogin using localStorage data

--- auth-01-auth-component/src/app/auth/auth.service.ts
+++ auth-01-auth-component/src/app/auth/auth.service.ts
@@ -54,6 +54,27 @@ export class AuthService {
       );
   }
 
+  autoLogin(){
+    const userData:{
+      email:string;
+      id:string;
+      _token:string;
+      _tokenExpirationDate:string;
+
+    } = JSON.parse(localStorage.getItem('userData'));
+    if(!userData){
+      return;
+    }
+
+    const loadedUser = new User(userData.email,userData.id,userData._token,new Date(userData._tokenExpirationDate));
+
+    //this is calling get token getter
+    if(loadedUser.token){
+      this.user.next(loadedUser);
+    }
+
+  }
+
   logout(){
     this.user.next(null);
     this.router.navigate(['/auth']);
@@ -67,6 +88,7 @@ export class AuthService {
   ) {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
+    localStorage.setItem('userData', JSON.stringify(user));
 
     this.user.next(user);
   }
